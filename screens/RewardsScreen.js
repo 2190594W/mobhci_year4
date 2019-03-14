cf4f4cc683fd3748a1115312e6c72b01645ef5d3
Chris Watson <2190594w@student.gla.ac.uk>
2019-03-14 00:11:07 +0000

    Completed workflow for purchasing a reward from a retailer.

--- screens/RewardsScreen.js
+++ screens/RewardsScreen.js
@@ -13,15 +13,16 @@ import {
   Modal,
 } from 'react-native';
 import { Icon } from 'expo';
+import { NavigationEvents } from 'react-navigation';
 
-import AnimateNumber from '@bankify/react-native-animate-number'
+import AnimateNumber from '@bankify/react-native-animate-number';
 
 const deviceWidth = Dimensions.get('window').width;
 
 export default class RewardsScreen extends React.Component {
   state = {
     currentTravelScore: 0,
-    modalOpacity: 0,
+    focused: true,
   };
 
   componentDidMount() {
@@ -42,7 +43,21 @@ export default class RewardsScreen extends React.Component {
         console.info("Updated score from storage");
       }
     } catch (error) {
-      console.log("Error with retrieving score: ", error);
+      console.error("Error with retrieving score: ", error);
+    }
+  };
+
+  _debitCurrScore = async (purchasePoints) => {
+    let currScore = this.state.currentTravelScore;
+    let newScore = currScore - purchasePoints;
+    if (newScore < 1) return;
+    try {
+      await AsyncStorage.setItem('@xPlore_Store:currScore', newScore.toFixed(1));
+      this.setState({
+        currentTravelScore: newScore,
+      });
+    } catch (error) {
+      console.error("Error with storing: ", error);
     }
   };
 
@@ -71,10 +86,84 @@ export default class RewardsScreen extends React.Component {
     const rewardTitles = ["Greggs", "Costa", "Taco Mazama", "Pret a Manger",
       "SPT - Subway", "Tesco", "Starbucks", "Boots", "Waitrose", "Subway", "Pizza Express"
     ];
+    const slugTitles = ["greggs", "costa", "taco_mazama", "pret_a_manger",
+      "spt_subway", "tesco", "starbucks", "boots", "waitrose", "subway", "pizza_express"
+    ];
     const rewardPckgs = {
-      "Greggs": []
+      "greggs": [
+        {"Name": "Ham & Cheese Baguette", "Points": 52600, "Picture": require('../assets/images/rewardPics/greggsHamCheeseBag.png')},
+        {"Name": "Mexican Chicken Baguette", "Points": 51800, "Picture": require('../assets/images/rewardPics/greggsMexChknBag.png')},
+        {"Name": "Tandoori Chicken Baguette", "Points": 50300, "Picture": require('../assets/images/rewardPics/greggsTandooriChknBag.png')},
+        {"Name": "Roast Chicken Salad Baguette", "Points": 35900, "Picture": require('../assets/images/rewardPics/greggsChknSub.png')}
+      ],
+      "costa": [],
+      "taco_mazama": [],
+      "pret_a_manger": [],
+      "spt_subway": [],
+      "tesco": [],
+      "starbucks": [],
+      "boots": [],
+      "waitrose": [],
+      "subway": [],
+      "pizza_express": [],
+    };
+
+    const rewardProds = {
+      "greggs": [],
+      "costa": [],
+      "taco_mazama": [],
+      "pret_a_manger": [],
+      "spt_subway": [],
+      "tesco": [],
+      "starbucks": [],
+      "boots": [],
+      "waitrose": [],
+      "subway": [],
+      "pizza_express": [],
     };
     const rewards = [];
+
+    for (var brandCount = 0; brandCount < slugTitles.length; brandCount++) {
+      let brandPckgs = rewardPckgs[slugTitles[brandCount]];
+      for (var pckgCount = 0; pckgCount < brandPckgs.length; pckgCount++) {
+        let currPckg = brandPckgs[pckgCount];
+        rewardProds[slugTitles[brandCount]].push(
+          <View
+            style={styles.productContainer}
+            key={`rewardProd${slugTitles[brandCount]}${pckgCount}`}
+          >
+            <View
+              style={styles.productNamePointsContainer}
+            >
+              <Text style={styles.productName}>{currPckg["Name"]}</Text>
+              <Text style={styles.productPrice}>Price: <Text style={[{fontWeight: 'bold'}, (this.state.currentTravelScore > currPckg["Points"]) ? {color: '#165139'} : {color: '#960000'}]}>{currPckg["Points"].toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Text></Text>
+            </View>
+            <View
+              style={styles.productImageContainer}
+            >
+              <Image
+                source={currPckg["Picture"]}
+                style={styles.productPicture}
+              />
+            </View>
+            <View
+              style={styles.productPurchaseContainer}
+            >
+              <TouchableOpacity
+                key={`rewardProd${slugTitles[brandCount]}${pckgCount}Purchase`}
+                disabled={(this.state.currentTravelScore > currPckg["Points"]) ? false : true}
+                style={[styles.productPurchase, (this.state.currentTravelScore > currPckg["Points"]) ? {} : {backgroundColor: '#165139'}]}
+                onPress={() => {
+                  this._debitCurrScore(currPckg["Points"]);
+                }}
+              >
+                <Icon.Ionicons name={(this.state.currentTravelScore > currPckg["Points"]) ? 'ios-basket' : 'ios-battery-dead'}  size={30} color="#ffffff"/>
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      }
+    }
 
     for (var rewardCount = 0; rewardCount < rewardLogos.length; rewardCount++) {
       let rewardID = `reward${rewardCount}`;
@@ -94,7 +183,6 @@ export default class RewardsScreen extends React.Component {
         </TouchableOpacity>,
         <Modal
           key={`reward${rewardCount}Modal`}
-          style={styles.rewardModal}
           transparent={true}
           animationType="slide"
           transparent={true}
@@ -120,7 +208,6 @@ export default class RewardsScreen extends React.Component {
               >
                 {rewardTitles[rewardCount]}
               </Text>
-
               <TouchableOpacity
                 style={styles.rewardModalCloseBtn}
                 onPress={() => {
@@ -128,6 +215,13 @@ export default class RewardsScreen extends React.Component {
                 }}>
                 <Icon.Ionicons name={'ios-close'}  size={50} color="#aaaaaa"/>
               </TouchableOpacity>
+              <ScrollView>
+                <View
+                  style={styles.rewardModalProductsScroll}
+                >
+                  {rewardProds[slugTitles[rewardCount]]}
+                </View>
+              </ScrollView>
             </View>
           </View>
         </Modal>
@@ -137,6 +231,9 @@ export default class RewardsScreen extends React.Component {
 
     return (
       <View style={styles.container}>
+        <NavigationEvents
+          onDidFocus={payload => this._getLastScore()}
+        />
         <View
           style={styles.scoreValue}
         >
@@ -232,8 +329,6 @@ const styles = StyleSheet.create({
     minWidth: deviceWidth * 0.45,
     minHeight: deviceWidth * 0.45,
   },
-  rewardModal: {
-  },
   rewardModalViewDismiss: {
     height: "40%",
     opacity: 0.0,
@@ -273,5 +368,69 @@ const styles = StyleSheet.create({
         left: "41%",
       }
     }),
-  }
+  },
+  rewardModalProductsScroll: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: '5%',
+    marginBottom: '40%',
+  },
+  productContainer: {
+    backgroundColor:'#ffffff',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+    	width: 0,
+    	height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+    width: deviceWidth * 0.86,
+    height: deviceWidth * 0.4,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "5%",
+    padding: "3%",
+    flexDirection: 'row',
+  },
+  productNamePointsContainer: {
+    flex: 2.5,
+    justifyContent: 'center',
+  },
+  productName: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  productPrice: {
+  },
+  productPictureContainer: {
+    flex: 3,
+    justifyContent: 'center',
+  },
+  productPicture: {
+    borderRadius: 10,
+    resizeMode: 'contain',
+    maxWidth: deviceWidth * 0.35,
+    maxHeight: deviceWidth * 0.35,
+    minWidth: deviceWidth * 0.2,
+    minHeight: deviceWidth * 0.1,
+    margin: "2%",
+    top: 'auto',
+    bottom: 'auto',
+  },
+  productPurchaseContainer: {
+    flex: 1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  productPurchase: {
+    backgroundColor:'#3bcd91',
+    borderRadius: 10,
+    width: "70%",
+    height: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
